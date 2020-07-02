77c1882a7fd3967c175e4688bbd85365c4986d53
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-07-02 17:00:30 +0530

    Update script.js

--- 17 Color Game Project/script.js
+++ 17 Color Game Project/script.js
@@ -1,10 +1,42 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+	this.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor(); 
+	colorDisplay.textContent = pickedColor;
+	for(var i=0;i<colors.length;++i){
+		squares[i].style.backgroundColor = colors[i];
+		var k = i+colors.length;
+		squares[k].style.display = "none";
+	}
+});
+
+hardBtn.addEventListener("click", function(){
+	this.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor(); 
+	colorDisplay.textContent = pickedColor;
+	for(var i=0;i<colors.length;++i){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	for(var i=3;i<colors.length;++i){
+		squares[i].style.display = "block";
+	}
+});
 
 resetButton.addEventListener("click", function(){
 	//generate all new colors
