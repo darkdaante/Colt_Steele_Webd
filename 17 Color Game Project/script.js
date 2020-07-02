a7792225951d6facf71eb2cc8b770693e8cfa68f
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-07-02 12:49:43 +0530

    Update script.js

--- 17 Color Game Project/script.js
+++ 17 Color Game Project/script.js
@@ -1,16 +1,9 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -25,8 +18,10 @@ for(var i=0;i<squares.length;++i){
 		//compare color to picked color
 		if(clickedColor===pickedColor){
 			messageDisplay.textContent = "Correct!";
+			h1.style.backgroundColor = clickedColor;
 			changeColors(clickedColor);
-		}else{
+		}
+		else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
 		}
@@ -44,4 +39,27 @@ function changeColors(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//add num random colors to array
+	for(var i=0;i<num;++i){
+		//get the random colors and push into arr
+		arr.push(randomColor());
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a red from 0-255
+	var red=Math.floor(Math.random()*256);
+	//pick a green from 0-255
+	var green=Math.floor(Math.random()*256);
+	//pick a blue from 0-255
+	var blue=Math.floor(Math.random()*256);
+	//"rgb(r, g, b)"
+	return "rgb("+red+", "+green+", "+blue+")";
 }
