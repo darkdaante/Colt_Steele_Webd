02efe1ff06d4811444132547a0c0f4f9a632649b
darkdaante <45766636+darkdaante@users.noreply.github.com>
2020-07-07 10:59:16 +0530

    Add files via upload

---
+++ 24 Intro To Node.js/IntrotoNPM/DemoApp/app.js
@@ -0,0 +1,5 @@
+var something = require('cat-me');
+var jokes = require('knock-knock-jokes');
+
+console.log(something());
+console.log(jokes());
---
+++ 24 Intro To Node.js/echo.js
@@ -0,0 +1,8 @@
+function echo(str, number) {
+	for (var i = 0; i < number; ++i) {
+		console.log(str);
+	}
+}
+
+echo('ECHO!!!', 10);
+echo('Tater Tots', 3);
